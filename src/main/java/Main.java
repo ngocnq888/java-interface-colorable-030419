c79d64cbb4dfdfa34cb030705c61a5c7001d3356
ngocnq888 <ngoc.nq.vn@gmail.com>
2019-04-03 05:54:23 +0700

    commit: update one element Square to array Square in Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Square firstSquare = new Square(2.0,"Hinh Vuong");
-        System.out.println(firstSquare.toString());
-        firstSquare.howToColor();
+        Square[] arrSquare = new Square[10];
+        for (int i = 0; i < arrSquare.length; i++) {
+            arrSquare[i] = new Square(Math.random() * 3, " Hinh Vuong " + i);
+        }
+        for (Square indexArr : arrSquare) {
+            System.out.println(indexArr.toString());
+            indexArr.howToColor();
+        }
     }
 }
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,25 +1,32 @@
 public class Square implements Colorable {
     public double size;
     public String name;
-    public Square(){}
-    public Square( double size, String name){
+
+    public Square() {
+    }
+
+    public Square(double size, String name) {
         this.size = size;
         this.name = name;
     }
-    public double getArea(){
-        return this.size*this.size;
+
+    public double getArea() {
+        return this.size * this.size;
     }
-    public  double getPerimeter(){
-        return this.size*4;
+
+    public double getPerimeter() {
+        return this.size * 4;
     }
+
     @Override
-    public void howToColor(){
+    public void howToColor() {
         System.out.println("Color all four sides");
     }
+
     @Override
-    public String toString(){
-        String str = this.name + " - " + this.size + " - " + this.getArea() + " - " + this.getPerimeter();
-        return  str;
+    public String toString() {
+        String str = " Name: " + this.name + " - Size: " + this.size + " - Area: " + this.getArea() + " - Perimeter: " + this.getPerimeter();
+        return str;
     }
 
 }
